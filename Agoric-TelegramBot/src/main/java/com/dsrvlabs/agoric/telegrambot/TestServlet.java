292584b7eb49dc4f5fda10d53033b299654f1935
Jongkwang Kim <kim@jongkwang.com>
2021-03-31 17:27:54 +0900

    Test servet and db

--- Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/TestServlet.java
+++ Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/TestServlet.java
@@ -1,11 +1,17 @@
 package com.dsrvlabs.agoric.telegrambot;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.dsrvlabs.common.db.CommonDao;
+import com.dsrvlabs.common.util.ServletUtil;
 
 /**
  * Servlet implementation class TestServlet
@@ -27,7 +33,21 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.getWriter().append("##### Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		HashMap<String, String> p = ServletUtil.paramToHashMapUtf8(request);
+		
+		String resultText = "";
+		
+		CommonDao dao = new CommonDao();
+		ArrayList list = dao.commonSelectList("DbMapper.TestSelect", p);
+		resultText += list.get(0).toString();
+		
+		HashMap map = (HashMap)list.get(0);
+		System.out.println(map.get("address"));
+		resultText += map.get("address");
+		
+		response.getWriter().append("##### Served at: " + resultText);
 	}
 
 }
