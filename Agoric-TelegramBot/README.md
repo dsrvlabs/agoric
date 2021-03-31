7bd348a8e84981f9356fdab112a07476db4cb541
JongKwang <kim@jongkwang.com>
2021-04-01 03:31:22 +0900

    Telegrambot development (#2)
    AgoricTelegramBot development

--- Agoric-TelegramBot/README.md
+++ Agoric-TelegramBot/README.md
@@ -3,11 +3,12 @@
 Agoric TelegramBot provides information from Agoric Network as Telegram Bot. now, you can check the validators' Node Status.
 
 ## Agoric TelegramBot - Features
-- Provides an alarm when there is a problem with my Validator
-- Validator list (TBA)
-- Account information (TBA)
+- Latest Block Height
+- Active Validators
+- Online Voting Power
+- Provides an alarm when there is a problem with my Validator (TBA)
 
 ## Telegram Link
-- http://t.me/AgoricTelegramBot
+- https://t.me/AgoricTelegramBot
 
-[Sample Image] [Sample Image]
+
--- Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/WebHookReceiver.java
+++ Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/WebHookReceiver.java
@@ -1,8 +1,6 @@
 package com.dsrvlabs.agoric.telegrambot;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
@@ -13,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-import com.dsrvlabs.common.db.CommonDao;
 import com.dsrvlabs.common.util.Discord;
 import com.dsrvlabs.common.util.MyString;
 import com.dsrvlabs.common.util.ServletUtil;
@@ -36,127 +33,80 @@ public class WebHookReceiver extends HttpServlet {
 		response.setContentType("text/html; charset=utf-8");
 		HashMap<String, String> p = ServletUtil.paramToHashMapUtf8(request);
 		
-		Discord.sendMsg("Test", "### Start : " + p.toString());
-		
 		// request info : fromId, text
-		String body = MyString.getBodyFormHttpRequest(request);	// Telegram 의 WebHook 은 body 에 jsonText 가 담겨서 오기 때문에 body를 읽는다.
+		String body = MyString.getBodyFormHttpRequest(request);
 		ObjectMapper jacksonMapper = new ObjectMapper();
 		HashMap map = jacksonMapper.readValue(body, HashMap.class);	// jsonText to HashMap
-		String fromId = ((HashMap)((HashMap)map.get("message")).get("from")).get("id").toString();	// 봇에게 메세지를 보내는사람의 Telegram ID
-		String cmd = ((HashMap)map.get("message")).get("text").toString();	// 봇에게 보낸 메세지
-		
-		Discord.sendMsg("Test", "### Text : " + cmd + " : " + fromId);
+		String fromId = ((HashMap)((HashMap)map.get("message")).get("from")).get("id").toString();
+		String cmd = ((HashMap)map.get("message")).get("text").toString();
 		
 		String msg = null;
 		if( cmd.equals("/start") || cmd.equals("/help") ) {
 			caseStartOrHelp(fromId);
 			
-		} else if ( cmd.equals("/blockheight") ) {
-			caseBlockHeight(fromId);
+		} else if ( cmd.equals("/BlockchainData") ) {
+			goSeleniumWorker(fromId, cmd);
 			
-		} else if ( cmd.equals("/list") ) {
-			caseList(fromId);
+		} else if ( cmd.equals("/ValidatorList") ) {
+			goSeleniumWorker(fromId, cmd);
 			
-		} else if ( cmd.startsWith("/watch ") ) {
+		} else if ( cmd.startsWith("/agoricv") ) {
+			goSeleniumWorker(fromId, cmd);
+			
+		} else if ( cmd.equals("/Watch") ) {
 			caseWatch(fromId, cmd);
 			
 		} else {
 			caseElse(fromId);
 		}
 		
-		Discord.sendMsg("Test", "### END");
+		//Discord.sendMsg("Test", "### END");
 		
 		response.setStatus(HttpServletResponse.SC_OK);
 	}
 
+	private void caseWatch(String fromId, String cmd) {
+		String msg;
+		msg = "Sorry. This feature is still under development.\n\n";
+		msg += "*Features*\n";
+		msg += "- Check every 3 minutes\n";
+		msg += "- Push alarm in case of failurel\n";
+		TelegramMsgSender.sendMsgToChannel(fromId, msg);
+	}
+
 	private void caseElse(String fromId) {
 		String msg;
-		msg = "Sorry. I did not understand your command. Please use the following commans.\n";
+		msg = "Sorry. I did not understand your command. Please use the following commans.\n\n";
 		msg += "*Features*\n";
-		msg += "/start - get help manual\n";
-		msg += "/help - get help manual\n";
-		msg += "/price - get price info\n";
-		msg += "/list - get validator list\n";
-		msg += "/watch 1 - watch the first validator of list. when validator commission rate is chaanged, you'll receive a info\n";
-		msg += "/watchingList - get my all watching list\n";
-		msg += "/watchingCancel - cancel all watching\n";
+		msg += "/start - Get help manual\n";
+		msg += "/help - Get help manual\n";
+		msg += "/BlockchainData - Get Agoric blockchain data\n";
+		msg += "/ValidatorList - Get validator data\n";
+		msg += "/ValidatorAddress - Get validator data\n";
+		msg += "/agoricvaloper1ns570lyx8lxevgtva6xdunjp0d35y3z32w3z6c - Sample\n";
+		msg += "/Watch - Watch node status and push notification\n";
 		TelegramMsgSender.sendMsgToChannel(fromId, msg);
 	}
 
-	private void caseWatch(String fromId, String text) {
-		String msg;
-		CommonDao dao = new CommonDao();
-		String validatorNum = text.split(" ")[1];
-		HashMap<String, String> paramWatch = new HashMap<String, String>();
-		
-		paramWatch.put("validatorNum", validatorNum);
-		ArrayList<HashMap> listResult = dao.commonSelectList("WebMapper.TelegramWebHookReceiver_selectValidatorInfo", paramWatch);
-		
-		String name = listResult.get(listResult.size()-1).get("name").toString();
-		String address = listResult.get(listResult.size()-1).get("address").toString();
-		
-		paramWatch.put("telegramId", fromId);
-		paramWatch.put("address", address);
-		
-		try {
-			dao.commonInsert("WebMapper.TelegramWebHookReceiver_insertTelegramWatch", paramWatch);
-		} catch (Exception e) {
-			// 중복인 경우 아무작업 하지 않음.
-		}
-		
-		msg = "*Watching below validator*\n\n";
-		msg += validatorNum + " : " + name + " is watched";
-		
-		TelegramMsgSender.sendMsgToChannel(fromId, msg);
-	}
 
-	private void caseList(String fromId) {
-		String msg;
-		CommonDao dao = new CommonDao();
-		HashMap result = dao.commonSelectOne("WebMapper.TelegramWebHookReceiver_listSum", null);
-		ArrayList<HashMap> listResult = dao.commonSelectList("WebMapper.TelegramWebHookReceiver_list", null);
+	private void goSeleniumWorker(String fromId, String cmd) {
 		
-		BigDecimal totalToken = new BigDecimal(result.get("totalToken").toString());
-		System.out.println( totalToken.toPlainString() );
-		
-		HashMap row = null;
-		BigDecimal token = null;
-		BigDecimal votingPower = null;
-		BigDecimal commission = null;
-		msg = "*Validator List*\n";
-		for( int i=0; i<listResult.size(); i++ ) {
-			row = listResult.get(i);
-			token = new BigDecimal(row.get("tokens").toString());
-			votingPower = token.divide(totalToken, 8, BigDecimal.ROUND_UP).multiply(new BigDecimal("100")).divide(BigDecimal.ONE, 2, BigDecimal.ROUND_UP);
-			commission = new BigDecimal(row.get("commission").toString()).multiply(new BigDecimal("100")).divide(BigDecimal.ONE, 0, BigDecimal.ROUND_UP);
-			
-			msg += (i + 1) + " : " + row.get("name").toString() + " (Voting Power : " + votingPower.toPlainString() + "%, Commission Rate : " + commission.toPlainString() + "%)\n";
-		}
-		
-		TelegramMsgSender.sendMsgToChannel(fromId, msg);
-	}
-	
-
-
-	private void caseBlockHeight(String fromId) {
-		
-		SeleniumWorker worker = new SeleniumWorker(fromId, null);
-		
-		TelegramMsgSender.sendMsgToChannel(fromId, "Start caseBlockHeight");
+		SeleniumWorker worker = new SeleniumWorker(fromId, cmd);
+		TelegramMsgSender.sendMsgToChannel(fromId, "Please wait");
 		worker.run();
-		TelegramMsgSender.sendMsgToChannel(fromId, "End caseBlockHeight");
 	}
 
 	private void caseStartOrHelp(String fromId) {
 		String msg;
-		msg = "Hello, I'm the Agoric TelegramBot! \n";
+		msg = "Hello, Welcome to *Agoric TelegramBot!* \n\n";
 		msg += "*Features*\n";
-		msg += "/start - get help manual\n";
-		msg += "/blockheight - get block height\n";
-		msg += "/price - get price info\n";
-		msg += "/list - get validator list\n";
-		msg += "/watch 1 - watch the first validator of list. when validator commission rate is changed, you'll receive a info\n";
-		msg += "/watchingCancel - cancel all watching\n";
+		msg += "/start - Get help manual\n";
+		msg += "/help - Get help manual\n";
+		msg += "/BlockchainData - Get Agoric blockchain data\n";
+		msg += "/ValidatorList - Get validator data\n";
+		msg += "/ValidatorAddress - Get validator data\n";
+		msg += "/agoricvaloper1ns570lyx8lxevgtva6xdunjp0d35y3z32w3z6c - Sample\n";
+		msg += "/Watch - Watch node status and push notification\n";
 		TelegramMsgSender.sendMsgToChannel(fromId, msg);
 	}
 }
