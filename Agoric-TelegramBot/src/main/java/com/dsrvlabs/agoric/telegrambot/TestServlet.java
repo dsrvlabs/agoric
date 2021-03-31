c5c18d5b310b0b2d9aad55615cdc96c763e31113
Jongkwang Kim <kim@jongkwang.com>
2021-03-31 17:51:48 +0900

    fix a bug

--- Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/TestServlet.java
+++ Agoric-TelegramBot/src/main/java/com/dsrvlabs/agoric/telegrambot/TestServlet.java
@@ -16,7 +16,7 @@ import com.dsrvlabs.common.util.ServletUtil;
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/TestServlet")
+@WebServlet("/server/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
